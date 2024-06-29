b2b57a8c888dff7c2c9a25bcd6a166cdab4d1f7c
Marst313 <dlegoinyoman1@gmail.com>
2024-06-29 11:13:09 +0700

    debuging

--- src/components/admin/ModalTambahKegiatan.jsx
+++ src/components/admin/ModalTambahKegiatan.jsx
@@ -1,20 +1,37 @@
-import React, { useEffect } from 'react';
-import emailjs from 'emailjs-com';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import emailjs from "emailjs-com";
+import { useDispatch, useSelector } from "react-redux";
 
-import { iconClose } from '../../images/icons/';
-import { convertDateString, convertHoursToSecond, convertTime } from '../../utils/function';
-import { getDataKegiatan, newDataKegiatan, setEditKegiatan, setNewActivity, setOpenModal, updateDataKegiatan } from '../../features/activity/activity';
+import { iconClose } from "../../images/icons/";
+import {
+  convertDateString,
+  convertHoursToSecond,
+  convertTime,
+} from "../../utils/function";
+import {
+  getDataKegiatan,
+  newDataKegiatan,
+  setEditKegiatan,
+  setNewActivity,
+  setOpenModal,
+  updateDataKegiatan,
+} from "../../features/activity/activity";
 
 const ModalTambahKegiatan = () => {
   const dispatch = useDispatch();
-  const { singleDataKegiatan, edit, openModalNewActivity, newKegiatan } = useSelector((store) => store.activity);
+  const { singleDataKegiatan, edit, openModalNewActivity, newKegiatan } =
+    useSelector((store) => store.activity);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (newKegiatan.judulKegiatan === '' || newKegiatan.deskripsiKegiatan === '' || newKegiatan.waktuMulai === '' || newKegiatan.waktuSelesai === '') {
-      console.log('All fields cant be empty');
+    if (
+      newKegiatan.judulKegiatan === "" ||
+      newKegiatan.deskripsiKegiatan === "" ||
+      newKegiatan.waktuMulai === "" ||
+      newKegiatan.waktuSelesai === ""
+    ) {
+      console.log("All fields cant be empty");
       return;
     }
 
@@ -70,12 +87,13 @@ const ModalTambahKegiatan = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
 
-    setDataKegiatan({ ...newKegiatan, [name]: value });
+    dispatch(setNewActivity({ ...newKegiatan, [name]: value }));
   };
 
   useEffect(() => {
     if (edit) {
-      const { date, description, title, waktuMulai, waktuSelesai } = singleDataKegiatan || {};
+      const { date, description, title, waktuMulai, waktuSelesai } =
+        singleDataKegiatan || {};
 
       dispatch(
         setNewActivity({
@@ -84,69 +102,118 @@ const ModalTambahKegiatan = () => {
           waktuMulai: convertTime(waktuMulai),
           waktuSelesai: convertTime(waktuSelesai),
           tanggalKegiatan: convertDateString(date),
-        })
+        }),
       );
     } else {
       dispatch(
         setNewActivity({
-          judulKegiatan: '',
-          deskripsiKegiatan: '',
-          waktuMulai: '',
-          waktuSelesai: '',
-          tanggalKegiatan: '',
-        })
+          judulKegiatan: "",
+          deskripsiKegiatan: "",
+          waktuMulai: "",
+          waktuSelesai: "",
+          tanggalKegiatan: "",
+        }),
       );
     }
   }, [edit, singleDataKegiatan]);
 
   return (
-    <div className={`w-full  h-full bg-white/20 backdrop-blur-sm  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2    justify-center items-center ${openModalNewActivity ? 'flex' : 'hidden'}`}>
-      <div className="lg:w-1/2 h-fit px-10 py-10 bg-white shadow-custom rounded-xl relative mt-10 lg:mt-0">
-        <h1 className="text-darkGreen font-medium text-2xl">{edit ? 'Edit' : 'Tambah'} Kegiatan</h1>
+    <div
+      className={`absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 transform items-center justify-center bg-white/20 backdrop-blur-sm ${openModalNewActivity ? "flex" : "hidden"}`}
+    >
+      <div className="shadow-custom relative mt-10 h-fit rounded-xl bg-white px-10 py-10 lg:mt-0 lg:w-1/2">
+        <h1 className="text-darkGreen text-2xl font-medium">
+          {edit ? "Edit" : "Tambah"} Kegiatan
+        </h1>
 
-        <form className="flex flex-col justify-center gap-3 mt-5 " onSubmit={handleSubmit}>
-          <div className="w-full  flex flex-col text-darkGreen gap-2">
+        <form
+          className="mt-5 flex flex-col justify-center gap-3"
+          onSubmit={handleSubmit}
+        >
+          <div className="text-darkGreen flex w-full flex-col gap-2">
             <label htmlFor="judulKegiatan" className="font-light">
               Judul Kegiatan
             </label>
-            <input type="text" name="judulKegiatan" id="judulKegiatan" className="focus:outline-none focus:ring-0 border-grey rounded-lg" onChange={handleChange} value={newKegiatan.judulKegiatan} />
+            <input
+              type="text"
+              name="judulKegiatan"
+              id="judulKegiatan"
+              className="border-grey rounded-lg focus:outline-none focus:ring-0"
+              onChange={handleChange}
+              value={newKegiatan.judulKegiatan}
+            />
           </div>
 
-          <div className="w-full  flex flex-col text-darkGreen gap-2">
+          <div className="text-darkGreen flex w-full flex-col gap-2">
             <label htmlFor="deskripsiKegiatan" className="font-light">
               Deskripsi Kegiatan
             </label>
-            <input type="text" name="deskripsiKegiatan" id="deskripsiKegiatan" className="focus:outline-none focus:ring-0 border-grey rounded-lg" onChange={handleChange} value={newKegiatan.deskripsiKegiatan} />
+            <input
+              type="text"
+              name="deskripsiKegiatan"
+              id="deskripsiKegiatan"
+              className="border-grey rounded-lg focus:outline-none focus:ring-0"
+              onChange={handleChange}
+              value={newKegiatan.deskripsiKegiatan}
+            />
           </div>
 
           <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="w-full  flex flex-col text-darkGreen gap-2">
+            <div className="text-darkGreen flex w-full flex-col gap-2">
               <label htmlFor="waktuMulai" className="font-light">
                 Waktu Mulai
               </label>
-              <input type="time" name="waktuMulai" id="waktuMulai" className="focus:outline-none focus:ring-0 border-grey rounded-lg" onChange={handleChange} value={newKegiatan.waktuMulai} />
+              <input
+                type="time"
+                name="waktuMulai"
+                id="waktuMulai"
+                className="border-grey rounded-lg focus:outline-none focus:ring-0"
+                onChange={handleChange}
+                value={newKegiatan.waktuMulai}
+              />
             </div>
-            <div className="w-full  flex flex-col text-darkGreen gap-2">
+            <div className="text-darkGreen flex w-full flex-col gap-2">
               <label htmlFor="waktuSelesai" className="font-light">
                 Waktu Selesai
               </label>
-              <input type="time" name="waktuSelesai" id="waktuSelesai" className="focus:outline-none focus:ring-0 border-grey rounded-lg" onChange={handleChange} value={newKegiatan.waktuSelesai} />
+              <input
+                type="time"
+                name="waktuSelesai"
+                id="waktuSelesai"
+                className="border-grey rounded-lg focus:outline-none focus:ring-0"
+                onChange={handleChange}
+                value={newKegiatan.waktuSelesai}
+              />
             </div>
           </div>
 
-          <div className="w-full  flex flex-col text-darkGreen gap-2">
+          <div className="text-darkGreen flex w-full flex-col gap-2">
             <label htmlFor="tanggalKegiatan" className="font-light">
               Tanggal Kegiatan
             </label>
-            <input type="date" name="tanggalKegiatan" id="tanggalKegiatan" className="focus:outline-none focus:ring-0 border-grey rounded-lg" onChange={handleChange} value={newKegiatan.tanggalKegiatan} />
+            <input
+              type="date"
+              name="tanggalKegiatan"
+              id="tanggalKegiatan"
+              className="border-grey rounded-lg focus:outline-none focus:ring-0"
+              onChange={handleChange}
+              value={newKegiatan.tanggalKegiatan}
+            />
           </div>
 
-          <button type="submit" className="text-white bg-lightGreen focus:ring-4 focus:outline-none  font-medium rounded-full text-sm w-32 px-8 py-2 text-center self-center mt-5">
-            {edit ? 'Edit' : 'Tambah'}
+          <button
+            type="submit"
+            className="bg-lightGreen mt-5 w-32 self-center rounded-full px-8 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+          >
+            {edit ? "Edit" : "Tambah"}
           </button>
         </form>
-        <button type="button" className="absolute top-0 right-0 mt-3 mr-3" onClick={handleCloseModal}>
-          <img src={iconClose} alt="icon close" className="w-8 " />
+        <button
+          type="button"
+          className="absolute right-0 top-0 mr-3 mt-3"
+          onClick={handleCloseModal}
+        >
+          <img src={iconClose} alt="icon close" className="w-8" />
         </button>
       </div>
     </div>
