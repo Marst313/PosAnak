e098908d2109ce16c2023523114679537c92a1e2
Marst313 <dlegoinyoman1@gmail.com>
2024-07-05 02:26:12 +0700

    debug again

---
+++ src/components/SidebarChatbot.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { chat, faq, pengaturan, logoWhite, defaultProfile } from "../images/";
+
+import {
+  arrowLeft,
+  iconChatBotBook,
+  arrowtop,
+  arrowbottom,
+} from "../images/icons";
+const SidebarChatbot = ({
+  isOpen,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  setIsOpen,
+}) => {
+  // Dropdown navbar
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <aside className="mr-5 flex w-[25%] flex-col">
+      {/* Header ASIDE */}
+      <div className="flex items-center justify-start gap-10">
+        <Link to="/dashboard/menu">
+          <img src={arrowLeft} alt="back icon" />
+        </Link>
+
+        <Link className="flex items-center gap-5" to="/dashboard/menu">
+          <img src={logoWhite} alt="logo putih" className="w-10" />
+          <h1 className="text-2xl text-white">Posanak</h1>
+        </Link>
+
+        <img
+          src={iconChatBotBook}
+          alt="logo buku"
+          className="w-10 cursor-pointer"
+          onClick={toggleSidebar}
+        />
+      </div>
+      {/* Header ASIDE */}
+
+      <div className="mb-2 mt-10 h-[70%]">
+        <ul>
+          <li className="my-2 flex cursor-pointer" onClick={toggleDropdown}>
+            <img
+              src={isOpen ? arrowtop : arrowbottom}
+              alt="arrow image"
+              className="mx-2"
+            />
+            <p>Menu Utama</p>
+          </li>
+          {isOpen && (
+            <ul className="absolute mt-2 w-72 rounded-lg bg-white p-2 shadow-lg">
+              <li className="text-darkGreen p-2">Coming Soon</li>
+              <li className="text-darkGreen p-2">Coming Soon</li>
+              <li className="text-darkGreen p-2">Coming Soon</li>
+            </ul>
+          )}
+          <li
+            className={`my-5 flex h-10 items-center rounded-xl bg-[#036346] transition-transform duration-500 ${
+              isOpen ? "translate-y-36" : "translate-y-0"
+            }`}
+          >
+            <img src={chat} alt="chat image" className="mx-2 h-6 w-6" />
+            <p className="text-white">Chatbot</p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <div className="flex">
+          <img src={faq} alt="faq image" className="h-5 w-5" />
+          <p className="mx-2 text-sm text-white">FAQ</p>
+        </div>
+
+        <div className="my-5 flex">
+          <img src={pengaturan} alt="pengaturan image" className="h-5 w-5" />
+          <p className="mx-2 text-sm text-white">Pengaturan</p>
+        </div>
+
+        <div className="rounded-xl bg-[#036346] p-2">
+          <section className="flex p-2">
+            <img
+              src={defaultProfile}
+              alt="user images profile"
+              className="mx-2 h-10 w-10 rounded-full"
+            />
+            <div className="mx-2">
+              <p className="text-sm text-white">Sumarni</p>
+              <p className="text-sm text-white/50">Sumarni@gmail.com</p>
+            </div>
+          </section>
+          <button className="h-10 w-full rounded-xl bg-white">
+            <p className="text-semibold">Membuat ChatBot Baru</p>
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default SidebarChatbot;
---
+++ src/images/index.js
@@ -0,0 +1,33 @@
+import adddata from "./adddata.png";
+import chat from "./chat.png";
+import contohProfile from "./contohProfile.jpeg";
+import defaultProfile from "./defaultProfile.png";
+import dokter from "./dokter.png";
+import existData from "./existdata.png";
+import faq from "./faq.png";
+import hampirStunting from "./hampirStunting.png";
+import heroBerita from "./hero-berita.png";
+import heroDashboard from "./hero-dashboard.png";
+import heroLogin2 from "./hero-login2.png";
+import logoPosyandu from "./logo-posyandu.webp";
+import logoWhite from "./logoWhite.png";
+import pengaturan from "./pengaturan.png";
+import vitaminBayi from "./vitaminBayi.png";
+
+export {
+  adddata,
+  chat,
+  contohProfile,
+  defaultProfile,
+  dokter,
+  existData,
+  faq,
+  hampirStunting,
+  heroBerita,
+  heroDashboard,
+  heroLogin2,
+  logoPosyandu,
+  pengaturan,
+  vitaminBayi,
+  logoWhite,
+};
